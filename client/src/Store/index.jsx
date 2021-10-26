e915eb4655337f34671f43dc1ffac08eeaefa6af
HEVERHD <hever11.hdgd@gmail.com>
2021-10-26 12:16:26 -0500

    create Store

---
+++ client/src/Store/index.jsx
@@ -0,0 +1,8 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import reducer from '../Redux/reducer';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
+
+export default store;
