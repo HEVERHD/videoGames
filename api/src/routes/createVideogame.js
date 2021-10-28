5cc56cdb82e3aa67962cb6060c95dbc0d8882fa2
HEVERHD <hever11.hdgd@gmail.com>
2021-10-27 19:25:48 -0500

    rutas rediseñadas

--- api/src/routes/createVideogame.js
+++ api/src/routes/createVideogame.js
@@ -1,30 +1,32 @@
 const { Router } = require('express');
-const { Videogame, Genre } = require('../db.js');
+const { Videogame, Genre, videogames_genre } = require('../db.js');
 
 const router = Router();
 
 //Recibe la data colectada desde el formulario por el body
 // Creo el videojuego en la db
 
-router.post('/', async (req, res) => {
-  const { name, description, image, released, rating, platforms, genres } = req.body;
+router.post('/', async (req, res, next) => {
+	const { name, description, image, released, rating, platforms, genres } =
+		req.body;
 
-  let platformString = platforms.join(', ')
+	// let platformString = platforms.join(', ');
 
-  let gameCreated = await Videogame.create({
-    name,
-    description,
-    image, 
-    released,
-    rating,
-    platforms: platformString,
-  })
+	let gameCreated = await Videogame.create({
+		name: name,
+		description: description,
+		image: image,
+		released: released,
+		rating: rating,
+		platforms: platforms,
+	});
 
-  genres.forEach(async (G) => {
-      let genresGame = await Genre.findOne({ where: { name: G } })
-      await gameCreated.addGenre(genresGame)
-  })
-    res.send('Videogame created successfully!')
+	genres.forEach(async (G) => {
+		let genresGame = await Genre.findOne({ where: { name: G } });
+		console.log(genresGame);
+		await gameCreated.addGenre(genresGame.id);
+	});
+	res.send('Videogame created successfully!');
 });
 
 module.exports = router;
