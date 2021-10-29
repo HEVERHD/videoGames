02d3c6e528e778dac8f83706a3b7bb55882310f0
HEVERHD <hever11.hdgd@gmail.com>
2021-10-29 18:43:27 -0500

    created store

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import NotFound from '../../components/NotFound/NotFound';
 import './Card.css';
 
-function Card({ data }) {
+function Card({data}) {
 	return (
 		<div className='card'>
 			<Link to={`/videogames/${data.id}`}>
 				{data.image === null || !data.image ? (
 					<NotFound image={'noimage'} />
 				) : (
-					<img className='imgs' src={data.image} alt={data.name} />
+					<img className='img' src={data.image} alt={data.name} />
 				)}
 			</Link>
 			<div className='textCard'>
