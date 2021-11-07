c2e84a4bcdf8f7d17ab3f3a0df7bbe34f8b24a85
HEVERHD <hever11.hdgd@gmail.com>
2021-11-07 03:22:20 -0500

    testing

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,53 +1,55 @@
-import React, { useState } from "react";
-import { Link, NavLink } from "react-router-dom";
-import "./Navbar.css"
+import React, { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import './Navbar.css';
 
 function NavBar() {
-    const [name, setName] = useState("");
+	const [name, setName] = useState('');
 
-    function handleSubmit(e) {
-        e.preventDefault();
-        setName("");
-    }
+	function handleSubmit(e) {
+		e.preventDefault();
+		setName('');
+	}
 
-    return (
-        <div className="navBar">
-            <div className="videogames">
-                <Link to="/">
-                    <h3>Videogames!</h3>
-                </Link>
-            </div>
-            <div className="home">
-                <Link to="/home">
-                    <h3>Home</h3>
-                </Link>
-            </div>
-            <div className="searchbar">
-                <form onSubmit={(e) => handleSubmit(e)}>
-                    <input
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    placeholder="Search videogame..."
-                    type="text"
-                    ></input>
-                    <NavLink to={`/results/${name}`}>
-                        <button name="name" type="submit"> Go! </button>
-                    </NavLink>
-                </form>
-            </div>
-            <div className="create">
-                <Link to="/create">
-                <h3>Create</h3>
-                </Link>
-            </div>
-            <div className="about">
-                <Link to="/about">
-                <h3>About</h3>
-                </Link>
-            </div>
-        </div>
-    );
+	return (
+		<div className='navBar'>
+			<div className='videogames'>
+				<Link to='/'>
+					<h3>Inicio</h3>
+				</Link>
+			</div>
+			<div className='home'>
+				<Link to='/home'>
+					<h3>Home</h3>
+				</Link>
+			</div>
+			<div className='searchbar'>
+				<form onSubmit={(e) => handleSubmit(e)}>
+					<input
+						value={name}
+						onChange={(e) => setName(e.target.value)}
+						placeholder='Search videogame...'
+						type='text'
+					></input>
+					<NavLink to={`/results/${name}`}>
+						<button name='name' type='submit'>
+							{' '}
+							Go!{' '}
+						</button>
+					</NavLink>
+				</form>
+			</div>
+			<div className='create'>
+				<Link to='/create'>
+					<h3>Create</h3>
+				</Link>
+			</div>
+			<div className='about'>
+				<Link to='/about'>
+					<h3>About</h3>
+				</Link>
+			</div>
+		</div>
+	);
 }
-
 
 export default NavBar;
